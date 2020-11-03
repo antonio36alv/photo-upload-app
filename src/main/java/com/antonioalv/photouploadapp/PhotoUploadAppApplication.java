435a248e560c673f830bc3cf3f2a5b22c7f97760
antonio36alv <ant.36alv@gmail.com>
2020-11-03 15:12:18 -0500

    initial commit

---
+++ src/main/java/com/antonioalv/photouploadapp/PhotoUploadAppApplication.java
@@ -0,0 +1,13 @@
+package com.antonioalv.photouploadapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoUploadAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoUploadAppApplication.class, args);
+	}
+
+}
